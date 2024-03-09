35f84475d16136bc50ec2ed017170c7afc39be51
Ramana Kumar <ramana@member.fsf.org>
2024-03-09 09:56:46 +0000

    Start drafting fee server

---
+++ run.js
@@ -0,0 +1,93 @@
+import { ethers } from 'ethers'
+import express from 'express'
+
+const provider = new ethers.JsonRpcProvider(process.env.RPC || 'http://localhost:8545')
+
+const port = process.env.PORT || 8080
+
+const feeContractAddresses = {
+  17000: '0x272347F941fb5f35854D8f5DbDcEdef1A515dB41'
+}
+
+const acceptedTokensByChain = {
+  17000: { blockNumber: 0, includedLogs: new Set(), current: new Set(), ever: new Set() }
+}
+
+const paymentsByChain = {
+  17000: { blockNumber: 0, includedLogs: new Set(), paymentsByAddress: new Map() }
+}
+
+const feeContracts = {}
+
+const feeContractAbi = [
+  'function weth() view returns (address)',
+  'function acceptedTokens(address) view returns (bool)',
+  'event Pay(address indexed user, address indexed token, uint256 indexed amount)',
+  'event SetToken(address indexed token, bool indexed accepted)'
+]
+
+const MAX_QUERY_RANGE = 10000
+
+const finalizedBlockNumber = await provider.getBlock('finalized').then(b => b.number)
+
+for (const [chainId, address] of Object.entries(feeContractAddresses)) {
+  const feeContract = new ethers.Contract(address, feeContractAbi, provider)
+  feeContracts[chainId] = feeContract
+  const acceptedTokens = acceptedTokensByChain[chainId]
+  acceptedTokens.current.add(await feeContract.weth())
+  acceptedTokens.ever.add(await feeContract.weth())
+  while (acceptedTokens.blockNumber < finalizedBlockNumber) {
+    const min = acceptedTokens.blockNumber
+    const max = Math.min(min + MAX_QUERY_RANGE, finalizedBlockNumber)
+    await feeContract.queryFilter('SetToken', min, max).then(logs => {
+      for (const {transactionHash, transactionIndex, args} of logs) {
+        if (!acceptedTokens.includedLogs.has(`${transactionHash}:${transactionIndex}`)) {
+          acceptedTokens.includedLogs.add(`${transactionHash}:${transactionIndex}`)
+          acceptedTokens.current[args.accepted ? 'add' : 'delete'](args.token)
+          if (args.accepted) acceptedTokens.ever.add(args.token)
+        }
+      }
+    })
+    acceptedTokens.blockNumber = max
+  }
+  // TODO: add listener for SetToken
+}
+
+// TODO: fill paymentsByChain and add listener
+
+const app = express()
+
+const addressRe = '0x[0-9a-fA-F]{40}'
+const addressRegExp = new RegExp(addressRe)
+
+const fail = (res, statusCode, body) => {
+  res.status(statusCode).send(body)
+}
+
+app.get(`/:chainId(\\d+)/:address(${addressRe})/payments`,
+  async (req, res, next) => {
+    try {
+      const feeContract = feeContracts[req.params.chainId]
+      if (!feeContract) return fail(404, 'unknown chainId')
+      const tokens = (typeof req.query.token == 'string' ? [req.query.token] : req.query.token) || []
+      if (tokens.some(t => !addressRegExp.test(t)))
+        return fail(400, 'invalid fee token address')
+      const acceptedTokens = acceptedTokensByChain[req.params.chainId]
+      if (tokens.some(t => !acceptedTokens.ever.has(t)))
+        return fail(400, 'fee token was never accepted')
+      if (!tokens.length)
+        tokens = Array.from(acceptedTokens.current.values())
+      const payments = paymentsByChain[req.params.chainId].paymentsByAddress[req.params.address]
+      const result = {}
+      tokens.forEach(t => result[t] = [])
+      if (!payments) return res.status(404).json(result)
+      for (const log of payments) {
+        if (tokens.includes(log.token))
+          result[log.token].append({amount: log.amount, timestamp: log.timestamp, tx: log.tx})
+      }
+      return res.status(200).json(result)
+    }
+    catch (e) { next(e) }
+})
+
+app.listen(port)
